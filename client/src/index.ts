ecab836d08a87e4138b93ff4e28aa73fd5020642
Pattadon Bao-ngern <pattadonb8@hotmail.com>
2021-01-14 12:05:17 +0700

    deconstruct 🧱 and change 🔍 scale to resize

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,31 +1,22 @@
 import 'phaser'
 
-type scaleMode = 'FIT' | 'SMOOTH'
-type scaleSize = {
-  WIDTH: number
-  HEIGHT: number
-  MAX_WIDTH: number
-  MAX_HEIGHT: number
-}
-
-const scaleSize: scaleSize = {
-  WIDTH: 1024,
-  HEIGHT: 576,
-  MAX_WIDTH: 1536,
-  MAX_HEIGHT: 864,
-}
-let SCALE_MODE: scaleMode = 'SMOOTH'
+const [DEFAULT_WIDTH, DEFAULT_HEIGHT, MAX_WIDTH, MAX_HEIGHT] = [
+  1024,
+  576,
+  1536,
+  864,
+]
 
 const config: Phaser.Types.Core.GameConfig = {
   backgroundColor: '#ffffff',
   scale: {
     parent: 'app',
-    mode: Phaser.Scale.NONE,
-    width: scaleSize.WIDTH,
-    height: scaleSize.HEIGHT,
-  }
+    mode: Phaser.Scale.RESIZE,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT,
+  },
 }
 
 window.addEventListener('load', () => {
-  const game = new Phaser.Game(config)
+  const game: Phaser.Game = new Phaser.Game(config)
 })
