ff6f255650f470f468c58d231d5bcde8f560af6a
Pattadon Bao-ngern <pattadonb8@hotmail.com>
2021-03-11 23:31:32 +0700

    handle players list in lobby

--- server/src/socket/socketController.ts
+++ server/src/socket/socketController.ts
@@ -3,20 +3,27 @@ import { Server, Socket } from 'socket.io'
 
 export default (server: http.Server) => {
   const io = new Server(server)
+  let playerList: any[] = []
 
   io.on('connection', (socket: Socket) => {
     // client connected handling
-    console.log(`client connected, id: ${socket.id}`)
-    const playerList = []
+    // console.log(`client connected, id: ${socket.id}`)
+    socket.emit('player-list', playerList)
 
     socket.on('join-room', (name: string) => {
       io.emit('join-room', name)
 
-      playerList.push({id: socket.id, name: name})
+      playerList = playerList.filter((player) => player.id !== socket.id)
+      playerList.push({ id: socket.id, name: name })
+      io.emit('player-list', playerList)
     })
 
     socket.on('disconnect', (reason) => {
       console.log(`client disconnect: ${reason}, id: ${socket.id}`)
+
+      // filter playerList
+      playerList = playerList.filter((player) => player.id !== socket.id)
+      io.emit('player-list', playerList)
     })
   })
 }
